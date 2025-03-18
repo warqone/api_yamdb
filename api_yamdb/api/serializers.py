b857b24f7b3133c4b139422a4db2e5c28597fad3
Denis Kuchmenko <denis.kuchm@gmail.com>
2025-03-18 23:20:09 +0700

    base

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import Title, Genre, Category
+
+
+class TitleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'year', 'category']
+        model = Title
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'slug']
+        model = Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'slug']
+        model = Category
