effa6adfc49b6286237f5a0b65dd944dc888b733
Ural Khalilov <uralkhalilov77@gmail.com>
2025-03-18 23:46:13 +0500

    #0.1.1

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from .models import Title, Genre, Category, Review, Comment
+
+
+class TitleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'year', 'category']
+        model = Title
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'slug']
+        model = Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'name', 'slug']
+        model = Category
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'text', 'author', 'score', 'pub_date']
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['id', 'text', 'author', 'pub_date']
+        model = Comment
