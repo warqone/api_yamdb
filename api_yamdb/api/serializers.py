8303990cb464606dfd3ba54ecddb4b9d1d303078
Denis Kuchmenko <denis.kuchm@gmail.com>
2025-03-19 19:46:59 +0700

    v.2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Title, Genre, Category
+from reviews.models import Title, Genre, Category
 
 
 class TitleSerializer(serializers.ModelSerializer):
